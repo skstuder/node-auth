d55a52d71372fdd293f7a5ef2e19000f6ed47bd8
Stephen Studer <stephen_studer@trekbikes.com>
2021-07-24 19:36:06 -0700

    set up environment variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./env.js";
 import { fastify } from "fastify";
 import pkg from "fastify-static";
 const { fastifyStatic } = pkg;
@@ -9,20 +10,22 @@ const __dirname = path.dirname(__filename);
 
 const app = fastify();
 
+console.log(process.env.MONGO_URL);
+
 async function startApp() {
   try {
     app.register(fastifyStatic, {
       root: path.join(__dirname, "public"),
     });
 
-    app.get("/", {}, (request, reply) => {
+    app.get("/hello-world", {}, (request, reply) => {
       reply.send({
         data: "hello world",
       });
     });
 
     await app.listen(4000);
-    console.log("🚀 Server Listening at port: 3000");
+    console.log("🚀 Server Listening at port: 4000");
   } catch (error) {
     console.log(error);
   }
