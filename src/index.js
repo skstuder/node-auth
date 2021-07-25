b3e443a79e57a12649b7e5f676ee8700f03bb3e0
Stephen Studer <stephen_studer@trekbikes.com>
2021-07-24 18:53:08 -0700

    add fastify, server reply and html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import "./db.js";
+import { fastify } from "fastify";
+import pkg from "fastify-static";
+const { fastifyStatic } = pkg;
+import path from "path";
+import { fileURLToPath } from "url";
 
-console.log("hello world");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = fastify();
+
+async function startApp() {
+  try {
+    app.register(fastifyStatic, {
+      root: path.join(__dirname, "public"),
+    });
+
+    app.get("/", {}, (request, reply) => {
+      reply.send({
+        data: "hello world",
+      });
+    });
+
+    await app.listen(4000);
+    console.log("🚀 Server Listening at port: 3000");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startApp();
