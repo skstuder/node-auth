5a9630878ded6acfc29f8224fb3e21e6e2842b19
Stephen Studer <stephen_studer@trekbikes.com>
2021-08-01 11:44:44 -0700

    setup login flow

---
+++ README.md
@@ -0,0 +1,2 @@
+Registration - salting, hashing, storing in db
+Authentication - compare hash to hash in db
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { connectDb } from "./db.js";
 import { registerUser } from "./accounts/register.js";
+import { authorizeUser } from "./accounts/authorize.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,7 +26,18 @@ async function startApp() {
           request.body.email,
           request.body.password
         );
-        console.log("userId", userId);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    app.post("/api/authorize", {}, async (request, reply) => {
+      try {
+        console.log(request.body.email, request.body.password);
+        const userId = await authorizeUser(
+          request.body.email,
+          request.body.password
+        );
       } catch (error) {
         console.error(error);
       }
