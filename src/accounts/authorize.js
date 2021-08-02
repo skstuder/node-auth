db123740d51ab6e71fede15c711d1619a2b4402a
Stephen Studer <stephen_studer@trekbikes.com>
2021-08-01 17:50:43 -0700

    compare saved password of like email with submitted password to authorize

--- src/accounts/authorize.js
+++ src/accounts/authorize.js
@@ -1,1 +1,17 @@
-export async function authorizeUser(email, password) {}
+import pkg from "bcryptjs";
+const { compare } = pkg;
+
+export async function authorizeUser(email, password) {
+  const { user } = await import("../user/user.js");
+  //look up user
+  const userData = await user.findOne({
+    "email.address": email,
+  });
+
+  //get user password
+  const savedPassword = userData.password;
+  //compare password with one in db
+  const isAuthorized = await compare(password, savedPassword);
+  //return boolean of if password is correct
+  return isAuthorized;
+}
