01b4bcdc7f210d8db74e2ff35720d6dabf4989f5
Stephen Studer <stephen_studer@trekbikes.com>
2021-10-01 14:34:39 -0700

    set up UI route for email verification, make call to api server

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,4 +1,6 @@
+import https from "https";
 import { fastify } from "fastify";
+import fetch from "cross-fetch";
 import fastifyStaticPkg from "fastify-static";
 const { fastifyStatic } = fastifyStaticPkg;
 import path from "path";
@@ -14,6 +16,37 @@ async function startApp() {
     app.register(fastifyStatic, {
       root: path.join(__dirname, "public"),
     });
+
+    app.get("/verify/:email/:token", {}, async (request, reply) => {
+      try {
+        const { email, token } = request.params;
+
+        const values = { email, token };
+
+        const httpsAgent = new https.Agent({
+          rejectUnauthorized: false,
+        });
+
+        const res = await fetch("https://api.nodeauth.dev/api/verify", {
+          method: "POST",
+          body: JSON.stringify(values),
+          credentials: "include",
+          agent: httpsAgent,
+          headers: { "Content-type": "application/json; charset=UTF-8" },
+        });
+        console.log("res", res);
+        reply.code(200).send("all is good!");
+        // send request to api
+      } catch (error) {
+        console.log(error);
+        reply.send({
+          data: {
+            status: "FAILED",
+          },
+        });
+      }
+    });
+
     const PORT = 5000;
     await app.listen(PORT);
     console.log(`🚀 Server Listening at port: ${PORT}`);
